eec43c7862c25c108d7fdeee8d7efc80594dcfaa
nicgil23 <nicgil@ucm.es>
2026-02-10 18:23:13 +0100

    Funcionalidad de extracción de links de una búsqueda de YouTube

--- src/1_Extraccion/Cv2_informacion_youtube.py
+++ src/1_Extraccion/Cv2_informacion_youtube.py
@@ -6,9 +6,9 @@ import subprocess
 import time
 
 """
-Script que tiene como objetivo conseguir información YouTube interceptando los paquetes de red
-usando una funcionalidad nativa que tiene DrissionPage, simplificando mucho la extracción de 
-datos. Además, usa TOR para evitar baneos de IP por conexiones excesivas al buscar en YouTube.
+Script que tiene como objetivo conseguir información YouTube scrapeando directamente de 
+Youtube. Además, usa TOR para evitar baneos de IP por conexiones excesivas al buscar, 
+ya que es una petición costosa.
 
 Requisitos:
 - Módulo `DrissionPage`.
@@ -32,7 +32,7 @@ user_agents = [
 def is_tor_running():
     for process in psutil.process_iter(attrs=['pid', 'name']):
         try:
-            if "tor.exe" in process.info['name'].lower():
+            if "tor.exe" == process.info['name'].lower():
                 return True
         except:
             continue
@@ -42,11 +42,11 @@ def start_tor():
     if not is_tor_running():
         # Cambiar PATH de donde hayáis descargado TOR
         TOR_PATH = r"C:\PROGRAMS\TOR\tor\tor.exe"
-        TORRC_PATH = r"C:\PROGRAMS\TOR\data\torrc"
+        #TORRC_PATH = r"C:\PROGRAMS\TOR\data\torrc"
 
         # Abrimos TOR
-        subprocess.Popen(TOR_PATH, "-f", TORRC_PATH, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        time.sleep(20)
+        subprocess.Popen(TOR_PATH, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(10)
 
 def renew_tor_ip(sesion):
     sesion.quit()
@@ -65,7 +65,7 @@ def main():
     start_tor()
     co = ChromiumOptions()
     co.set_user_agent(np.random.choice(user_agents))
-    co.set_argument('--proxy-server=socks5://127.0.0.1:9050') # Puerto que usar TOR
+    co.set_argument('--proxy-server=socks5://127.0.0.1:9050') # Puerto que usa TOR
     sesion = ChromiumPage(co)
 
     # url
@@ -74,18 +74,20 @@ def main():
     url = "https://www.youtube.com/results?search_query=" + cosa + "&sp=CAM%253D"
 
     # Navegamos
-    #sesion.listen.start(targets='oembed', method="GET", res_type="fetch")
-    sesion.get("https://check.torproject.org", interval=5)
-
-    """
+    # sesion.get("https://check.torproject.org", interval=5) Para comprobar que estamos metidos o no a red TOR
+    sesion.get(url)
     sesion.wait(5, 0.5)
-    res = sesion.listen.wait(count=1)
-    if 'youtube.com' in res.url:
-        print(f"Capturado con exito: {res.url}")
-        print(res.response.body)
     
+    feed_videos = sesion.ele('tag:ytd-two-column-search-results-renderer')
+    videos = feed_videos.eles("tag:ytd-video-renderer")
+    lista_enlaces = []
+    for v in videos:
+        enlace = v.ele('#thumbnail').attr('href')
+        lista_enlaces.append(enlace.split('&')[0])
+    
+    print(lista_enlaces)
+    sesion.wait(5, 0.5)
     sesion.quit()
-    """
 
 if __name__ == "__main__":
     main()
