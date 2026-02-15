e1ec9a1a837381967bd7dd78ecd35b11b7141214
nicgil23 <nicgil@ucm.es>
2026-02-15 16:14:22 +0100

    Optimización de main.py: ahora no se importan todos los archivos al comienzo de la ejecución, sino que se importan de manera dinámica cuando ejecutas un Script usando importlib

--- src/1_Extraccion/main.py
+++ src/1_Extraccion/main.py
@@ -1,51 +1,45 @@
 import os
 import sys
+import importlib
 
 # Para que pueda usar los ficheros importados que están dentro de Scripts (Z_funciones)
 sys.path.append(os.path.join(os.path.dirname(__file__), "Scripts"))
-
-import Scripts.A_lista_juegos
-import Scripts.B_informacion_juegos
-import Scripts.C1_informacion_youtube_busquedas
-import Scripts.C2_informacion_youtube_videos
-import Scripts.D_informacion_resenyas
-import Scripts.E_metadatos_imagenes
 
 scripts_info = {
     "A": {
         "fichero": "A_lista_juegos", 
         "salida": "steam_apps.json.gz",
-        "ejecutable": Scripts.A_lista_juegos.A_lista_juegos,
+        "ejecutable": "A_lista_juegos",
         "usar": False
     },
     "B": {
         "fichero": "B_informacion_juegos", 
         "salida": "info_steam_games.json.gz",
-        "ejecutable": Scripts.B_informacion_juegos.B_informacion_juegos,
+        "ejecutable": "B_informacion_juegos",
         "usar": False
     },
     "C1": {
         "fichero": "C1_informacion_youtube_busquedas", 
         "salida": "info_steam_youtube1.json.gz",
-        "ejecutable": Scripts.C1_informacion_youtube_busquedas.C1_informacion_youtube_busquedas,
+        "ejecutable": "C1_informacion_youtube_busquedas",
         "usar": False
     },
     "C2": {
         "fichero": "C2_informacion_youtube_videos", 
         "salida": "info_steam_youtube2.json.gz",
-        "ejecutable": Scripts.C2_informacion_youtube_videos.C2_informacion_youtube_videos,
+        "ejecutable": "C2_informacion_youtube_videos",
         "usar": False
     },
     "D": {
         "fichero": "D_informacion_resenyas", 
         "salida": "info_steam_resenyas.json.gz",
-        "ejecutable": Scripts.D_informacion_resenyas.D_informacion_resenyas,
+        "ejecutable": "D_informacion_resenyas",
         "usar": False
     },
     "E": {
         "fichero": "E_metadatos_imagenes", 
         "salida": "info_imagenes.json.gz",
-        "ejecutable": Scripts.E_metadatos_imagenes.E_metadatos_imagenes,
+        "ejecutable": "E_metadatos_imagenes",
         "usar": False
     }
 }
@@ -77,8 +71,13 @@ def ejecutar_scripts():
 
     for clave, info in sorted(scripts_info.items()):
         if info["usar"]:
-            print(f">> Ejecutando {clave}: {info['fichero']}...")
-            info["ejecutable"]()
+            print(f">> Importando y ejecutando {clave}: {info['fichero']}...")
+            
+            # Importamos el archivo que queremos ejecutar y lanzamos la función
+            modulo = importlib.import_module(f"Scripts.{info["fichero"]}")
+            funcion = getattr(modulo, info["ejecutable"])
+            funcion()
+
             alguno_ejecutado = True
             info["usar"] = not info["usar"]
     
