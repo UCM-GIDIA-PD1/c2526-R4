a49f79263da2974287b5205993a18a933dc6a2bd
JanMercado51 <jankrism@ucm.es>
2026-02-25 16:45:03 +0100

    test de transformacion

--- src/transformacion/games_info_transformacion.py
+++ src/transformacion/games_info_transformacion.py
@@ -5,20 +5,63 @@ de pandas creando columnas nuevas y eliminando columnas innecesarias.
 
 import pandas as pd
 import json
-import pathlib as Path
+from  pathlib import Path
+import gzip
+import os
 
-base_dir = Path().resolve().parents[1] # Nos situamos en la carpeta del proyecto
-data_dir = base_dir / 'data' # cd data
+ROOT_DIR = os.getcwd()
 
-# No usa variable de entorno pues la ruta será el archivo final con todos los juegos,
-# no partes individuales de cada identificador
-ruta = Path(data_dir / f'info_steam_games_3.json.gz')
+def _get_datapath():
+    data_path = ROOT_DIR / 'data'
+    return data_path
 
-if not ruta.exists():
-    raise FileNotFoundError(f'No se encuentra la ruta: {ruta}')
-
-with gzip.open(ruta, 'rt', encoding='UTF-8') as f:
-    data = json.load(f)
+def _read_jsonl(filepath):
+    with open(filepath, "rt", encoding="utf-8") as f:
+        data = [json.loads(line) for line in f if line.strip()]
+        return data
     
-df = pd.DataFrame(data['data'])
-df
+def _get_name(x):
+    if isinstance(x,dict):
+        return x.get("name")
+    else:
+        return None
+
+def _get_genres(x):
+    if not isinstance(x, dict):
+        return []
+    genres = x.get('genres', [])
+    if not isinstance(genres, list):
+        return []
+    return [g.get('description') for g in genres if isinstance(g, dict)]
+
+def _get_categories(x):
+    if not isinstance(x,dict):
+        return []
+    categories = x.get("categories", [])
+    if not isinstance(categories, list):
+        return []
+    return [c.get("description") for c in categories if isinstance(c, dict)]
+
+def _is_free(x):
+    if not isinstance(x,dict):
+        return []
+    price_ov = x.get("price_overview", [])
+    if not isinstance(price_ov, dict):
+        return []
+    return price_ov.get("initial") == 0
+        
+
+if __name__ == '__main__':
+    # Obtenemos el path a al archivo 'games_info.jsonl.gz' 
+    data_path = _get_datapath()
+    games_info_path = data_path / 'games_info.jsonl.gz'
+    games_info = _read_jsonl(games_info_path)
+
+    # Creamos el dataframe base
+    df = pd.DataFrame(games_info)
+    df = df.join(df["appdetails"].apply(pd.Series))
+
+
+
+    
+    
