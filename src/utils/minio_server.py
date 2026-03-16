a7c64805080a9e6cfee8af45fb2144fb7367368e
lucasosp <lucasosp@ucm.es>
2026-03-16 21:38:41 +0100

    Actualizada cabecera de notebook para que pueda leer de MinIO, además de algunos cambios en las funciones de MinIO para soportar nuestro sistema de carpetas

--- src/utils/minio_server.py
+++ src/utils/minio_server.py
@@ -5,6 +5,7 @@ Módulo que se encarga de la conexión con Minio.
 from minio import Minio
 from minio.error import S3Error
 from os import environ
+from src.utils.config import project_root
 
 def _minio_client():
     return Minio(endpoint = "minio.fdi.ucm.es",
@@ -47,11 +48,16 @@ def download_from_minio(download_path, filename = None):
         filename = download_path
 
     client = _minio_client()
-    name = filename.name if hasattr(filename, 'name') else filename
-    minio_path = f"grupo4/{name}"
+    
+    if hasattr(filename, 'name') and len(filename.parts) > 1:
+        name_in_minio = f"{filename.parent.name}/{filename.name}"
+    else:
+        name_in_minio = filename.name if hasattr(filename, 'name') else filename
+
+    minio_path = f"grupo4/{name_in_minio}"
+    
     try:
-        client.fget_object(bucket_name = "pd1", object_name = minio_path, file_path = download_path)
-        print("Se ha descargado el fichero correctamente")
+        client.fget_object(bucket_name="pd1", object_name=minio_path, file_path=download_path)
         return True
     except S3Error as e:
         print(f"Error de MinIO : {e}")
