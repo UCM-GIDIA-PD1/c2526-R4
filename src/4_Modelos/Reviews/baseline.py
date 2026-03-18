f4bf9b190eeec5b2a316dd426679ba3916682c51
anton-VK <antovlad@ucm.es>
2026-03-18 16:29:49 +0100

    Añadido baseline para el problema de predicción de la valoración de comentarios de Steam.

---
+++ src/4_Modelos/Reviews/baseline.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import wandb
+
+from src.utils.config import reviews
+from src.utils.files import read_file
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score, recall_score,f1_score
+
+
+def create_reviews_baseline():
+
+    run = wandb.init(
+        entity="pd1-c2526-team4",
+        project="Reviews", 
+        name="baseline_all_positive",
+        job_type="baseline"
+    )
+
+    df = read_file(reviews)
+    y_column = "is_positive"
+
+    train_df, test_df = train_test_split(df, test_size=0.20, random_state=42)
+
+    mayority = train_df[y_column].value_counts().idxmax()
+
+    y_true = test_df[y_column]
+    y_pred = [mayority] * len(y_true)
+
+
+    acurracy = accuracy_score(y_true, y_pred)
+    balanced_accuracy = balanced_accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+
+    wandb.log({
+        "accuracy" : acurracy,
+        "balanced_accuracy": balanced_accuracy,
+        "precision" : precision,
+        "recall" : recall,
+        "f1" : f1
+    })
+
+    run.finish()
+
+
+if __name__ == "__main__":
+    create_reviews_baseline()
