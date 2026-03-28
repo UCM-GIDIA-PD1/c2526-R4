0ef34748109f19792fbe12bac0a3303c4a0d1a64
JanMercado51 <jankrism@ucm.es>
2026-03-28 16:47:44 +0100

    Modelo sin imagenes xgboost

--- src/4_Modelos/Precios/utils_modelo_precios/preprocesamiento.py
+++ src/4_Modelos/Precios/utils_modelo_precios/preprocesamiento.py
@@ -1,6 +1,6 @@
-""""
+''''
 Módulo de preprocesamiento de dataframe de precios para los modelos de predicción de rango de precio de un juego.
-"""
+'''
 
 import pandas as pd
 import numpy as np
@@ -10,9 +10,13 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split    
+from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+
+
 
 def prices_dataframe() -> pd.DataFrame:
-    """
+    '''
     Lee el archivo de precios.parquet y devuelve el dataframe quitando las columnas que no se usarán para el modelo.
 
     Se eliminan las siguientes columnas:
@@ -21,17 +25,19 @@ def prices_dataframe() -> pd.DataFrame:
         - price_overview
         - v_resnet
         - v_convnext
-    """
+    '''
     df = read_file(prices)
-    assert df, 'Error archivo precios.parquet no encontrado'
+    assert df is not None, 'Error archivo precios.parquet no encontrado'
 
     df.drop(columns=['id','name','price_overview','v_resnet','v_convnext'], inplace=True)
+    df['release_year'] = df['release_year'].apply(lambda x : int(x))
+
     return df
 
 def train_val_test_split(X, y):
-    """
+    '''
     Dado un conjunto de datos X e y, realiza una división Train/Validation/Test de un 0.7/0.15/0.15.
-    """
+    '''
 
     X_train, X_temp, y_train, y_temp = train_test_split( X, y, test_size=0.3, random_state=42, stratify=y )
     X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp )
@@ -39,9 +45,9 @@ def train_val_test_split(X, y):
     return X_train, X_val, X_test, y_train, y_val, y_test
 
 def cluster_embedings(df : pd.DataFrame, emb_col : str) -> np.ndarray: 
-    """
+    '''
     Dado un dataFrame y una columna donde se encuentran los embeddings, devuelve un array resultado del clustering de esos embeddings.
-    """
+    '''
     kmeans = KMeans(random_state=42)
 
     embed = df[emb_col].apply(pd.Series)
@@ -50,9 +56,9 @@ def cluster_embedings(df : pd.DataFrame, emb_col : str) -> np.ndarray:
     return clusters
 
 def normalize_train_test(X_train, X_val, X_test):
-    """
+    '''
     Dados unos conjuntos de entrenamiento train y test los normaliza usando StandardScaler.
-    """
+    '''
     scaler = StandardScaler()
 
     X_train_scaled = scaler.fit_transform(X_train)
@@ -62,9 +68,9 @@ def normalize_train_test(X_train, X_val, X_test):
     return X_train_scaled, X_val_scaled, X_test_scaled
 
 def pca_train_test(X_train, X_val, X_test, n_comp = 0.9):
-    """
+    '''
     Dados unos conjuntos train y test realiza un pca sobre ellos para reducir dimensionalidad.
-    """
+    '''
     pca = PCA(n_components=n_comp, random_state=42)
 
     X_train_pca = pca.fit_transform(X_train)
@@ -72,3 +78,33 @@ def pca_train_test(X_train, X_val, X_test, n_comp = 0.9):
     X_test_pca = pca.transform(X_test)
 
     return X_train_pca, X_val_pca, X_test_pca
+
+def class_weights(y):
+    '''
+    Dado el conjunto y saca los pesos de las clases, usado para tratar el desbalance entre clases al clasificar.
+    '''
+    sample_weights = compute_sample_weight(class_weight='balanced', y=y)
+    return sample_weights
+
+
+def get_metrics(y_test, y_pred, classes=None):
+    '''
+
+    '''
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred, average='weighted')
+    rec = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+
+
+    print(f'Accuracy:  {acc}')
+    print(f'Precision: {prec}')
+    print(f'Recall:    {rec}')
+    print(f'F1 Score:  {f1}')
+    print(classification_report(y_test, y_pred, target_names=classes))
+
+    # 3. Matriz de Confusión Visual
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+
+    return {'accuracy': acc, 'precision': prec, 'recall': rec, 'f1': f1 }
--- src/4_Modelos/Precios/xgboost_base.py
+++ src/4_Modelos/Precios/xgboost_base.py
@@ -3,23 +3,100 @@ Modelo de XGBoost para la clasificación multiclase de rango de precio de un jue
 
 En este script se realizarán los siguientes modelos:
     - Modelo sin información de las imágenes
-    - Modelo con embeddings de imagenes
+    - Modelo con embeddings de imagenes (Procesado con un PCA)
 
 Dependencias:
     - precios.parquet
 """
 
-def _initial_preprocess():
-    """
-    
-    """
-    
-    
-    pass
 
+from .utils_modelo_precios.preprocesamiento import prices_dataframe, train_val_test_split, class_weights, get_metrics
+from sklearn.preprocessing import LabelEncoder
+import xgboost as xgb
+from sklearn.metrics import f1_score
+import optuna
+import wandb
+
+def model_noimg(df):    
+    # Hacemos encoding de la variable objetivo ya que no acepta str XGBoost
+    le = LabelEncoder()
+    df['price_range'] = le.fit_transform(df['price_range'])
+
+    # División Train, Validation, Test
+    y = df['price_range']
+    X = df.drop(columns=['price_range'])
+    X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(X, y)
+
+    sample_weights = class_weights(y_train)
+
+    def objective(trial):
+        param = {
+            'verbosity': 0,
+            'objective': 'multi:softprob',
+            'num_class': len(set(y_train)),
+            'n_estimators': trial.suggest_int('n_estimators', 100, 1000),
+            'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3, log=True),
+            'max_depth': trial.suggest_int('max_depth', 3, 10),
+            'subsample': trial.suggest_float('subsample', 0.5, 1.0),
+            'colsample_bytree': trial.suggest_float('colsample_bytree', 0.5, 1.0),
+            'gamma': trial.suggest_float('gamma', 1e-8, 1.0, log=True),
+            'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
+            'random_state': 42,
+            'device': 'cuda' if False else 'cpu',
+        }
+        model = xgb.XGBClassifier(**param)
+        model.fit(
+            X_train, y_train,
+            sample_weight=sample_weights,
+            eval_set=[(X_val, y_val)],
+            verbose=False
+        )
+        preds = model.predict(X_val)
+        score = f1_score(y_val, preds, average='weighted')        
+        return score
+
+    run = wandb.init(
+        entity="pd1-c2526-team4",
+        project="Precios", 
+        name='XGBoost-Base NoImg',
+        job_type='xgboost'
+    )
+
+    study = optuna.create_study(direction='maximize')
+    study.optimize(objective, n_trials=50)
+    
+    print(f"Mejor F1-Score: {study.best_value}")
+    print(f"Mejores parámetros: {study.best_params}")
+    best_params = study.best_params
+
+    final_model = xgb.XGBClassifier(
+        **best_params,
+        objective='multi:softprob',
+        num_class=len(le.classes_),
+        random_state=42
+    )
+    
+    final_model.fit(
+        X_train, y_train, 
+        sample_weight=sample_weights,
+        verbose=False
+    )
+
+    y_pred = final_model.predict(X_test)
+
+    metrics_dict = get_metrics(y_test, y_pred)
+    
+    run.log(metrics_dict)
+    run.finish()
 
 def xgboost_base():
-    pass
+    df = prices_dataframe()
+
+    df_noimg = df.drop(columns=['brillo', 'v_clip'])
+    model_noimg(df_noimg)
+
+    df_img = df.copy()
+
 
 if __name__ == '__main__':
     xgboost_base()
