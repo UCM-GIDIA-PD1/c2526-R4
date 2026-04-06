c489c917cfa86cffd87ad92891945f11864c4f48
JanMercado51 <jankrism@ucm.es>
2026-04-06 22:20:12 +0200

    Añadido KNN en evaluación

--- src/4_Modelos/Precios/evaluacion.py
+++ src/4_Modelos/Precios/evaluacion.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import RandomizedSearchCV
 import os
 import joblib
 import statsmodels.api as sm
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def catboostModel(df, table, model_path= 'data/models/catboostClustered.pkl'):
@@ -67,7 +68,42 @@ def xgboostUmap(df, table, model_path= 'data/models/xgboostumap.pkl'):
          raise FileNotFoundError
 
 def knnModel(df, table, model_path= 'data/models/knncompleteclusters.pkl'):
-    pass
+    y = df['price_range']
+    X = df.drop(columns=['price_range'])
+    X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(X, y)
+    X_train, X_val, X_test = cluster_embedings(X_train, X_val, X_test, emb_col='v_clip')
+
+    le = LabelEncoder()
+    y_train = le.fit_transform(y_train)
+    y_val   = le.transform(y_val)
+    y_test  = le.transform(y_test)
+
+    columnas_categoricas = ['Action','Adventure', 'Casual', 'Early Access', 'Indie', 'RPG', 'Simulation',
+    'Strategy', 'Co-op', 'Custom Volume Controls', 'Family Sharing',
+    'Full controller support', 'Multi-player', 'Online Co-op', 'Online PvP',
+    'Partial Controller Support', 'Playable without Timed Input', 'PvP',
+    'Remote Play Together', 'Shared/Split Screen', 'Single-player',
+    'Steam Achievements', 'Steam Cloud', 'Steam Leaderboards', 'Steam Trading Cards']
+
+    columnas_numericas = X_train.columns.difference(columnas_categoricas).tolist()
+    X_train, X_val, X_test = normalize_train_test(X_train, X_val, X_test, columnas_numericas)
+
+    if os.path.exists(model_path):
+        model = joblib.load(model_path)
+
+        y_pred = model.predict(X_test)
+        metrics_dict = get_metrics(y_test, y_pred)
+
+
+        table.add_data(
+            'K-NN Complete Clusters',
+            metrics_dict['accuracy'],
+            metrics_dict['f1-score'],
+            metrics_dict['precision'],
+            metrics_dict['recall']
+        )
+    else:
+         raise FileNotFoundError
 
 
 def evaluate_models():
@@ -85,8 +121,10 @@ def evaluate_models():
     catboostModel(df.copy(), table)
     print('Subiendo modelo XGBoost UMap')
     xgboostUmap(df.copy(), table)
-
-
+    print('Subiendo modelo KNN Complete Clusters')
+    knnModel(df.copy(), table)
+                
+                
     wandb.log({"comparative_table": table})
     print("Evaluación completada. Resultados en W&B.")
     run.finish()
