3745d7f2bdce0ec6e9030ae155dce10f5a665d3a
anton-VK <antovlad@ucm.es>
2026-04-07 00:06:30 +0200

    Añadido baseline y modelo de regresión logística usando gridsearchCV para la evaluación de los modelos del primer problema de reviews.

--- src/4_Modelos/Reviews/evaluacion.py
+++ src/4_Modelos/Reviews/evaluacion.py
@@ -15,6 +15,7 @@ from utils_modelo_reviews.preprocesamiento import train_val_test_split
 from tqdm import tqdm
 
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score, recall_score,f1_score
+from sklearn.model_selection import train_test_split
 
 from logistic_regression import preprocess
 
@@ -29,6 +30,29 @@ def evaluate_models():
     tqdm.pandas(desc="Limpiando texto")
     df = read_file(reviews)
     
+    
+    # Modelo baseline (moda)
+    y_column = "is_positive"
+
+    train_df, test_df = train_test_split(df, test_size=0.20, random_state=42)
+
+    mayority = train_df[y_column].value_counts().idxmax()
+
+    y_true = test_df[y_column]
+    y_pred = [mayority] * len(y_true)
+
+
+    accuracy = accuracy_score(y_true, y_pred)
+    balanced_accuracy = balanced_accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    
+    table = wandb.Table(columns=["Model", "Accuracy", "F1-score", "Balanced accuracy", "Recall", "Precision"])
+    table.add_data("baseline-mode", accuracy, f1, balanced_accuracy, recall, precision)
+    
+    
+    # Modelo de regresión logística
     X, y = preprocess(df)
     
     X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(X, y)
@@ -36,10 +60,10 @@ def evaluate_models():
     X_train_val = np.concatenate([X_train, X_val])
     y_train_val = np.concatenate([y_train, y_val])
     
-    model_path = "data/models/logistic_regression_optuna.pkl"
+    model_path_optuna = "data/models/logistic_regression_optuna.pkl"
     
-    if os.path.exists(model_path):
-        best_logistic_model = joblib.load(model_path)
+    if os.path.exists(model_path_optuna):
+        best_logistic_model = joblib.load(model_path_optuna)
         
         y_pred_test = best_logistic_model.predict(X_test)
 
@@ -49,12 +73,27 @@ def evaluate_models():
         recall= recall_score(y_test, y_pred_test)
         precision=  precision_score(y_test, y_pred_test)
         
-        table = wandb.Table(columns=["Model", "accuracy", "f1", "balanced_accuracy", "recall", "precision"])
+
         table.add_data("logistic_regression_optuna", accuracy, f1, balanced_accuracy, recall, precision)
         
+    model_path_gridsearch = "data/models/logistic_regression_gridsearch.pkl"
+    
+    if os.path.exists(model_path_gridsearch):
+        best_logistic_model = joblib.load(model_path_gridsearch)
         
-        wandb.log({"comparative_table": table})
-        print("Evaluación completada. Resultados en W&B.")
+        y_pred_test = best_logistic_model.predict(X_test)
+
+        accuracy = accuracy_score(y_test, y_pred_test)
+        f1 = f1_score(y_test, y_pred_test)
+        balanced_accuracy = balanced_accuracy_score(y_test, y_pred_test)
+        recall= recall_score(y_test, y_pred_test)
+        precision=  precision_score(y_test, y_pred_test)
+        
+
+        table.add_data("logistic_regression_gridsearch", accuracy, f1, balanced_accuracy, recall, precision)   
+
+    wandb.log({"comparative_table": table})
+    print("Evaluación completada. Resultados en W&B.")
 
     run.finish()
     
