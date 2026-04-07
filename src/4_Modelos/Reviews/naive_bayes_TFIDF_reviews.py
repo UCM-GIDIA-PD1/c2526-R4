b68f4981da7f30df59f69d2bf790deb6e48d0266
zhixianzucm <zhixianz@ucm.es>
2026-04-07 07:47:18 +0200

    modularización naive bayes tfidf

--- src/4_Modelos/Reviews/naive_bayes_TFIDF_reviews.py
+++ src/4_Modelos/Reviews/naive_bayes_TFIDF_reviews.py
@@ -1,67 +1,68 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score, recall_score, f1_score
+from utils_modelo_reviews.preprocesamiento import read_reviews, train_val_test_split, clean_text_lemma
+from tqdm import tqdm
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import ComplementNB
-from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
-import re
-from src.utils.config import reviews
-from tqdm import tqdm
-import wandb
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score, recall_score, f1_score
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+import optuna
 import nltk
-from utils_modelo_reviews.preprocesamiento import clean_text
-
+import wandb
 nltk.download('stopwords')
-
-run = wandb.init(
-        entity="pd1-c2526-team4",
-        project="Reviews", 
-        name="reviews-naivebayes-tfidf",
-        job_type="model"
-    )
-
-
 tqdm.pandas(desc="Limpiando texto")
+# run = wandb.init(
+#         entity="pd1-c2526-team4",
+#         project="Reviews", 
+#         name="reviews-naivebayes-cv",
+#         job_type="model"
+#     )
 
-df = pd.read_parquet(reviews)
 
-stop_words = set(stopwords.words("english"))
-ps = PorterStemmer()
+def preprocesar_texto(X_train, X_val, X_test):
+    X_train = [clean_text_lemma(review) for review in tqdm(X_train, desc = "Preprocesando entrenamiento")]
+    X_val = [clean_text_lemma(review) for review in tqdm(X_val, desc = "Preprocesando validacion")]
+    X_test = [clean_text_lemma(review) for review in tqdm(X_test, desc = "Preprocesando prueba")]
+    return X_train, X_val, X_test
 
-df["text"] = df["text"].progress_apply(lambda x : clean_text(x,ps,stop_words))
+def entrenar_modelo(X_train, y_train):
+    classifier = ComplementNB()
+    classifier.fit(X_train, y_train)
+    return classifier
 
-print("Entrenando modelo")
-X_train, X_test, y_train, y_test = train_test_split(df["text"], df["is_positive"], train_size=0.8, random_state=42)
 
-vectorizer = TfidfVectorizer(ngram_range=(1, 2), max_df=0.9, min_df=5)
-X_train = vectorizer.fit_transform(X_train) 
-X_test = vectorizer.transform(X_test)       
+def calcular_metricas(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    balanced_accuracy = balanced_accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
 
-classifier = ComplementNB()
-classifier.fit(X_train, y_train)
+    print("Accuracy:", accuracy)
+    print("Balanced accuracy:", balanced_accuracy)
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("F1-score:", f1)
+    return accuracy, balanced_accuracy, precision, recall, f1
 
-y_pred = classifier.predict(X_test)
+# wandb.log({
+#         "Accuracy": accuracy,
+#         "Balanced accuracy": balanced_accuracy,
+#         "Precision": precision,
+#         "Recall": recall,
+#         "F1-score": f1
+#     })
 
-print("Calculando métricas")
-accuracy = accuracy_score(y_test, y_pred)
-balanced_accuracy = balanced_accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
+# run.finish()
+if __name__ == "__main__":
+    df = read_reviews()
+    reviews = df["text"].to_list() # minusculas y solo caracteres alphanumericos y signos comunes de puntuacion
+    labels = df["is_positive"].to_list()
 
-print("Accuracy:", accuracy)
-print("Balanced accuracy:", balanced_accuracy)
-print("Precision:", precision)
-print("Recall:", recall)
-print("F1-score:", f1)
+    X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(reviews, labels)
 
-wandb.log({
-        "Accuracy": accuracy,
-        "Balanced accuracy": balanced_accuracy,
-        "Precision": precision,
-        "Recall": recall,
-        "F1-score": f1
-    })
-
-run.finish()
+    X_train, X_val, X_test = preprocesar_texto(X_train, X_val, X_test)
+    modelo, mejores_params = entrenar_modelo_con_optuna(X_train, y_train)
+    y_pred = modelo.predict(X_val)
+    calcular_metricas(y_val, y_pred)
+    print(mejores_params)
