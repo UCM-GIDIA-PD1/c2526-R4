0ff775e4678ed413f8220aa536744955ca994f1b
zhixianzucm <zhixianz@ucm.es>
2026-05-03 19:34:16 +0200

    Arreglado y añadido test a extracción de ids de videos

--- src_2/data_extraction/youtube_scaper.py
+++ src_2/data_extraction/youtube_scaper.py
@@ -2,6 +2,7 @@
 from DrissionPage import ChromiumPage, ChromiumOptions
 from random import choice, randint
 from src_2.config import BROWSER_PATH, USER_AGENTS, COMMON_RESOLUTIONS, TOR_SOCKS_PROXY
+from src_2.network.tor_manager import start_tor
 
 def new_configured_chromium_page():
     """
@@ -16,11 +17,11 @@ def new_configured_chromium_page():
     
     ancho, alto_base = choice(COMMON_RESOLUTIONS)
     alto = alto_base + randint(-20, 0)
-    co.set_argument(f'--window-size={ancho},{alto}')
+    co.set_argument(f"--window-size={ancho},{alto}")
 
     return ChromiumPage(co)
 
-def search_youtube(session: ChromiumPage, game_name: str, date: str):
+def get_video_ids(session: ChromiumPage, game_name: str, date: str):
     """
     Busca vídeos de un juego en YouTube anteriores a una fecha específica.
     
@@ -34,16 +35,34 @@ def search_youtube(session: ChromiumPage, game_name: str, date: str):
     session.get(url)
     
     try:
-        results_container = session.ele('tag:ytd-two-column-search-results-renderer')
+        results_container = session.ele("tag:ytd-two-column-search-results-renderer")
         video_elements = results_container.eles("tag:ytd-video-renderer")
         video_ids = []
         
         for v in video_elements:
-            href = v.ele('#thumbnail').attr('href')
-            if href and 'watch?v=' in href and 'shorts' not in href:
-                video_id = href.split('v=')[1].split('&')[0]
+            href = v.ele("#thumbnail").attr("href")
+            if href and "watch?v=" in href and "shorts" not in href:
+                video_id = href.split("v=")[1].split("&")[0]
                 video_ids.append({"id": video_id})
         return video_ids
     except Exception:
         return []
     
+def test_get_video_ids():
+    print("Test get_video_ids()--------------------")
+    game_name = "Apex Legends"
+    release_date = "2020-11-05"
+    
+    try:
+        start_tor() 
+        session = new_configured_chromium_page()
+        video_ids = get_video_ids(session, game_name, release_date)
+        assert isinstance(video_ids, list), "El resultado debe ser una lista."
+        print(f"Test exitoso: {video_ids}")
+    except Exception as e:
+        print(f"Test fallido: {e}")
+    finally:
+        session.quit()
+
+if __name__ == "__main__":
+    test_get_video_ids()
