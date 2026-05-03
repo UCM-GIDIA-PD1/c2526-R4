6166cd708398729b48a17f529b3996184df69a70
zhixianzucm <zhixianz@ucm.es>
2026-05-03 19:11:49 +0200

    extracción de ids de youtube y configuración de tor y chromium page

--- src_2/config.py
+++ src_2/config.py
@@ -1,5 +1,24 @@
 import os
+from pathlib import Path
+import platform
+# ------- Estructura de carpetas -------
+def project_root():
+    """Devuelve un objecto Path con la raíz del proyecto."""
+    current = Path(__file__).resolve()
 
+    for parent in current.parents:
+        if (parent / "pyproject.toml").exists():
+            current = parent
+            break
+    return current
+
+def config_files_folder():
+    """Devuelve un objecto Path con el directorio de config."""
+    path = project_root() / "config_files"
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+# ------- Variables de entorno -------
 def get_env_var(name):
     """Obtiene una variable de entorno y valida su existencia."""
     value = os.getenv(name)
@@ -14,3 +33,20 @@ def get_steam_api_key():
 def get_youtube_api_key():
     """Obtiene la clave de API de YouTube desde las variables de entorno."""
     return get_env_var("API_KEY_YT")
+
+# ------- Configuración de TOR --------
+TOR_CONTROL_PORT = 9051
+TORRC_PATH = config_files_folder() / "torrc"
+TOR_SOCKS_PROXY = "socks5://127.0.0.1:9050"
+BROWSER_PATH = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+
+# ------- Configuración de para ChromiumPage --------
+_sys = platform.system()
+if _sys == 'Windows':
+    USER_AGENTS = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36']
+elif _sys == 'Darwin':
+    USER_AGENTS = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36']
+else:
+    USER_AGENTS = ['Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36']
+
+COMMON_RESOLUTIONS = [(1920, 1080), (1366, 768), (1536, 864), (1440, 900)]
---
+++ src_2/data_extraction/youtube_scaper.py
@@ -0,0 +1,49 @@
+
+from DrissionPage import ChromiumPage, ChromiumOptions
+from random import choice, randint
+from src_2.config import BROWSER_PATH, USER_AGENTS, COMMON_RESOLUTIONS, TOR_SOCKS_PROXY
+
+def new_configured_chromium_page():
+    """
+    Configura y devuelve una nueva instancia de ChromiumPage con proxy TOR.
+    """
+    co = ChromiumOptions()
+    
+    co.set_browser_path(BROWSER_PATH)
+    co.set_user_agent(choice(USER_AGENTS))
+    co.set_argument(f'--proxy-server={TOR_SOCKS_PROXY}')
+    co.set_argument('--password-store=basic')
+    
+    ancho, alto_base = choice(COMMON_RESOLUTIONS)
+    alto = alto_base + randint(-20, 0)
+    co.set_argument(f'--window-size={ancho},{alto}')
+
+    return ChromiumPage(co)
+
+def search_youtube(session: ChromiumPage, game_name: str, date: str):
+    """
+    Busca vídeos de un juego en YouTube anteriores a una fecha específica.
+    
+    Returns:
+        list[dict]: Lista de IDs de vídeos encontrados.
+    """
+
+    query = f"%22{game_name.replace(" ", "+")}%22+Steam+game+before:{date}"
+    url = f"https://www.youtube.com/results?search_query={query}&sp=CAM%253D"
+
+    session.get(url)
+    
+    try:
+        results_container = session.ele('tag:ytd-two-column-search-results-renderer')
+        video_elements = results_container.eles("tag:ytd-video-renderer")
+        video_ids = []
+        
+        for v in video_elements:
+            href = v.ele('#thumbnail').attr('href')
+            if href and 'watch?v=' in href and 'shorts' not in href:
+                video_id = href.split('v=')[1].split('&')[0]
+                video_ids.append({"id": video_id})
+        return video_ids
+    except Exception:
+        return []
+    
