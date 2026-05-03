7a33609b8feeeb66c053112afbd69d40aac74d3e
lucasosp <lucasosp@ucm.es>
2026-05-03 20:55:20 +0200

    Bug fix que no permitía ejecutar la app desde fuera del contenedor con los nuevos cambios

--- src/D_Modelos/Reviews/utils/preprocesamiento.py
+++ src/D_Modelos/Reviews/utils/preprocesamiento.py
@@ -5,6 +5,7 @@ Módulo de preprocesamiento de dataframe de reviews para el análisis de los com
 from src.utils.files import read_file
 from src.utils.config import reviews, new_data_reviews
 import re
+import nltk
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
@@ -37,13 +38,30 @@ def read_new_data(minio={"minio_write": False, "minio_read": False}):
 
     return df
 
-def clean_text_stem(text, stemmer=PorterStemmer(), stop_words=set(stopwords.words("english"))):
+def _get_stopwords():
+    try:
+        return set(stopwords.words("english"))
+    except LookupError:
+        nltk.download('stopwords')
+        nltk.download('wordnet')
+        return set(stopwords.words("english"))
+
+def clean_text_stem(text, stemmer=None, stop_words=None):
     """Se queda solo lo que es texto, quitando stopwords y aplicando stemming"""
-    text = re.sub(r"[^a-z\s]", "", text)
+    if stemmer is None:
+        stemmer = PorterStemmer()
+    if stop_words is None:
+        stop_words = _get_stopwords()
+
+    text = re.sub(r"[^a-z\s]", "", text.lower()) # Añadido .lower() por seguridad
     return " ".join(stemmer.stem(word) for word in text.split() if word not in stop_words)
 
-
-def clean_text_lemma(text, lemmatizer=WordNetLemmatizer(), stop_words=set(stopwords.words("english"))):
+def clean_text_lemma(text, lemmatizer=None, stop_words=None):
     """Se queda solo lo que es texto, quitando stopwords y aplicando stemming"""
-    text = re.sub(r"[^a-z\s]", "", text)
+    if lemmatizer is None:
+        lemmatizer = WordNetLemmatizer()
+    if stop_words is None:
+        stop_words = _get_stopwords()
+
+    text = re.sub(r"[^a-z\s]", "", text.lower())
     return " ".join(lemmatizer.lemmatize(word) for word in text.split() if word not in stop_words)
