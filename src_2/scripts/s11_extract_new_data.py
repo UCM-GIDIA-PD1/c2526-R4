1e28a19b8cc286d4997cc66677cd13ab32301c29
zhixianzucm <zhixianz@ucm.es>
2026-05-05 18:43:41 +0200

    Movido carpeta Scripts dentro de src_2, para no molestar en la estructura del repositorio

--- src_2/scripts/s11_extract_new_data.py
+++ src_2/scripts/s11_extract_new_data.py
@@ -3,16 +3,16 @@ import pandas as pd
 from src_2.config import popularity_parquet, prices_parquet, reviews_parquet
 from src_2.io_manager import merge_and_save
 
-from scripts.s01_appid_list_full import main as update_census
-from scripts.s02_appid_list_sample import main as generate_sample
-from scripts.s03_steam_details import main as extract_details
-from scripts.s04_steam_reviews import main as extract_reviews
-from scripts.s05_youtube_video_ids import main as youtube_video_ids
-from scripts.s06_youtube_video_stats import main as youtube_video_stats
-from scripts.s07_image_features import main as image_features
-from scripts.s08_transform_popularity import main as transform_popularity
-from scripts.s09_transform_prices import main as transform_prices
-from scripts.s10_transform_reviews import main as transform_reviews
+from src_2.scripts.s01_appid_list_full import main as update_census
+from src_2.scripts.s02_appid_list_sample import main as generate_sample
+from src_2.scripts.s03_steam_details import main as extract_details
+from src_2.scripts.s04_steam_reviews import main as extract_reviews
+from src_2.scripts.s05_youtube_video_ids import main as youtube_video_ids
+from src_2.scripts.s06_youtube_video_stats import main as youtube_video_stats
+from src_2.scripts.s07_image_features import main as image_features
+from src_2.scripts.s08_transform_popularity import main as transform_popularity
+from src_2.scripts.s09_transform_prices import main as transform_prices
+from src_2.scripts.s10_transform_reviews import main as transform_reviews
 
 def prepare_version_paths(base_path):
     directory = base_path.parent
@@ -29,12 +29,16 @@ def prepare_version_paths(base_path):
     return old_path, new_path, final_path
 
 def main():
+    
     print("Iniciando pipeline completo")
     
     old_popularity, new_popularity, final_popularity = prepare_version_paths(popularity_parquet)
     old_prices, new_prices, final_prices = prepare_version_paths(prices_parquet)
     old_reviews, new_reviews, final_reviews = prepare_version_paths(reviews_parquet)
-
+    for final_path, old_path in [(final_popularity, old_popularity), (final_prices, old_prices), (final_reviews, old_reviews)]:
+        if final_path.exists():
+            shutil.move(final_path, old_path)
+            
     print("Ejecutando scripts de extracción")
     update_census()
     generate_sample()
